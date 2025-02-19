2e319c70e26a8b946acb5b119aea88c94ec3d7e0
tbonegaming344 <96313735+tbonegaming344@users.noreply.github.com>
2025-02-19 18:15:40 -0500

    fixed error with the npa embed on lgtyqz command

--- ZV(Misc)/DeckBuilders/lg.js
+++ ZV(Misc)/DeckBuilders/lg.js
@@ -63,23 +63,23 @@ Note: ${user.displayName} has ${decks.length} total decks in Tbot`,
     .setColor("Random");
     let noplayingallowed= new EmbedBuilder()
     .setTitle(`${result[5].noplayingallowed}`)
-    .setDescription(`${result[3].noplayingallowed}`)
-    .setFooter({text: `${result[2].noplayingallowed}`})
-          .addFields({
-            name: "Deck Type",
-            value: `${result[6].noplayingallowed}`,
-            inline: true
-          },{
-            name: "Archetype",
-            value: `${result[0].noplayingallowed}`,
-            inline: true
-          },{
-            name: "Deck Cost", 
-            value: `${result[1].noplayingallowed}`,
-            inline: true
-          })
-      .setColor("Random")
-      .setImage(`${result[4].noplayingallowed}`)
+	.setDescription(`${result[3].noplayingallowed}`)
+	.setFooter({text: `${result[2].noplayingallowed}`})
+				.addFields({
+					name: "Deck Type",
+					value: `${result[6].noplayingallowed}`,
+					inline: true
+				},{
+					name: "Archetype",
+					value: `${result[0].noplayingallowed}`,
+					inline: true
+				},{
+					name: "Deck Cost", 
+					value: `${result[1].noplayingallowed}`,
+					inline: true
+				})
+		.setColor("Random")
+		.setImage(`${result[4].noplayingallowed}`)
     const m = await message.channel.send({
       embeds: [lgtqyz],
       components: [row],

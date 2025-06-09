308d834e3bacc4e3e747a41eae4bc0f64c7b20b3
tbonegaming344 <96313735+tbonegaming344@users.noreply.github.com>
2025-06-09 09:50:15 -0400

    Updated Quicksand server link and removed pvzh archives on the tourneys command

--- ZV(Misc)/ZV(Misc)/tourneys.js
+++ ZV(Misc)/ZV(Misc)/tourneys.js
@@ -27,7 +27,7 @@ module.exports = {
       container.addTextDisplayComponents(tourneyText);
       const tourneyText2 = new TextDisplayBuilder().setContent([
         "# Gimmick Tournaments",
-        "PvZH Gimmick Tournaments is a server that exclusively hosts niche, unordinary tournaments in pvzh. In this server you can also find updates for when a pvzh tournament opens and closes their sign ups.", 
+        "PvZH Gimmick Tournaments is a server that exclusively hosts niche, unordinary tournaments in pvzh.", 
         "Tournaments are usually hosted once every 1 1/2 months"
       ].join('\n'));
     const gimmickTournaments = new ButtonBuilder()
@@ -49,30 +49,17 @@ module.exports = {
     const section3 = new SectionBuilder().addTextDisplayComponents(tourneyText3).setButtonAccessory(pvzhRevived);
     container.addSectionComponents(section3);
     const tourneyText4 = new TextDisplayBuilder().setContent([
-      "# pvzh archives", 
-      "Offers varied and experimental tournaments, that try to push pvzh competitive outside of the norm.",
-      "Tends to have one tournament active at any given time, with often a month or two till one tourney ends and another begins."
-    ].join('\n'));
-    const pvzhArchives = new ButtonBuilder()
-      .setLabel('pvzh archives')
-      .setStyle(ButtonStyle.Link)
-      .setURL('https://discord.gg/JtsYctnZTc')
-      .setEmoji('<:pvzharchives:1367922015004135484>')
-    const section4 = new SectionBuilder().addTextDisplayComponents(tourneyText4).setButtonAccessory(pvzhArchives);
-    container.setAccentColor(0xFF0000);
-    container.addSectionComponents(section4);
-    const tourneyText5 = new TextDisplayBuilder().setContent([
       "# Quicksand", 
       "Quicksand Tournaments is an elo system server, meaning that rather than participating in a bracket-style tournament, you can play at any time you like as long as there is another player who is down to play against you. Top 8 of each season (=3 months) participates in a special Playoffs Tournament. Hope to see you there!"
     ].join('\n'));
     const quicksand = new ButtonBuilder()
       .setLabel('Quicksand')
       .setStyle(ButtonStyle.Link)
-      .setURL('https://discord.gg/fFZBpVry')
+      .setURL('https://discord.gg/VFGVmsAVdB')
       .setEmoji('<:quicksand:1273834699072081920>')
-    const section5 = new SectionBuilder().addTextDisplayComponents(tourneyText5).setButtonAccessory(quicksand);
-    container.addSectionComponents(section5);
-    const tourneyText6 = new TextDisplayBuilder().setContent([
+    const section4 = new SectionBuilder().addTextDisplayComponents(tourneyText4).setButtonAccessory(quicksand);
+    container.addSectionComponents(section4);
+    const tourneyText5 = new TextDisplayBuilder().setContent([
       "# pvzhtwjizñë", 
       "Essentially a tournament-focused server that aims to invite both new and experienced players. We tend to have tournaments every few months, sometimes being “gimmick” tournaments with special rules, while other times being a regular tournament with two brackets"
     ].join('\n'));
@@ -81,8 +68,8 @@ module.exports = {
       .setStyle(ButtonStyle.Link)
       .setURL('https://discord.gg/7c9ybscBMN')
       .setEmoji('<:pvzhtw:1369013512709345372>')
-    const section6 = new SectionBuilder().addTextDisplayComponents(tourneyText6).setButtonAccessory(pvzhtwjizne);
-    container.addSectionComponents(section6);
+    const section5 = new SectionBuilder().addTextDisplayComponents(tourneyText5).setButtonAccessory(pvzhtwjizne);
+    container.addSectionComponents(section5);
     message.channel.send({
       components: [container],
       flags: MessageFlags.IsComponentsV2

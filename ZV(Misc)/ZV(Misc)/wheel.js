fd546ea28128db19ed560f8260359d04e5989a5c
tbonegaming344 <96313735+tbonegaming344@users.noreply.github.com>
2025-07-03 09:58:24 -0400

    added random hero to wheel command

--- ZV(Misc)/ZV(Misc)/wheel.js
+++ ZV(Misc)/ZV(Misc)/wheel.js
@@ -13,19 +13,48 @@ const {
         return message.author.send("This command is disabled in this server please use it in another server");
       }
       else{
-      const button = new ButtonBuilder()
-      .setLabel('Spin the Wheel')
+      const row= new ActionRowBuilder().addComponents(
+        new ButtonBuilder()
+        .setLabel('Spin the Wheel')
       .setCustomId('wheel-spin')
       .setStyle(ButtonStyle.Secondary)
-      .setEmoji('<:200w:1260951571840237628>');
-      const row = new ActionRowBuilder({
-        components: [button],
-    });
+      .setEmoji('<:200w:1260951571840237628>'),
+        new ButtonBuilder()
+          .setCustomId('plants')
+          .setLabel('Random Plant Hero')
+          .setStyle(ButtonStyle.Primary)
+          .setEmoji('<:200w:1260951571840237628>'),
+        new ButtonBuilder()
+          .setCustomId('zombies')
+          .setLabel('Random Zombie Hero')
+          .setStyle(ButtonStyle.Primary)
+          .setEmoji('<:200w:1260951571840237628>')
+      )
         const search = new EmbedBuilder()
         .setTitle("Wheel of Decks")
         .setDescription("Please use the button below to spin the wheel and get cards to build a deck with")
         .setColor("Random")
         await message.reply({embeds: [search], components: [row]})
     }
+    const filter = i => i.user.id === message.author.id;
+    const collector = message.channel.createMessageComponentCollector({ filter});
+    collector.on('collect', async i => {
+      const heroes = {
+        plantheroes: ["Green Shadow", "Wallknkight", "Spudow", "Grass Knuckles", "Rose", "Beta Carrotina", "Solar Flare", "Chompzilla", 
+          "Citron", "Night Cap", "Captain Combustible"
+        ], 
+        zombieheroes: ["Super Brainz", "impfinity", "Electric Boogaloo", "Professor Brainstorm", "Zmech", "Huge Giganticus",
+          "The Smash", "Rustbolt", "Brain Freeze", "Immorticia", "Neptuna"
+        ]
+      }
+      if(i.customId == 'plants'){
+        const randomHero = heroes.plantheroes[Math.floor(Math.random() * heroes.plantheroes.length)];
+        await i.reply({content: `Hello ${i.user.username}, you got **${randomHero}** as your random plant hero!`});
+      }
+      else if(i.customId == 'zombies'){
+        const randomHero = heroes.zombieheroes[Math.floor(Math.random() * heroes.zombieheroes.length)];
+        await i.reply({content: `Hello ${i.user.username}, you got **${randomHero}** as your random zombie hero!`});
+      }
+    })
   }
   }

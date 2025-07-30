932e0d95a21e0584827fe7ee82de8e95fbc2bbc1
tbonegaming344 <96313735+tbonegaming344@users.noreply.github.com>
2025-07-30 14:41:50 -0400

    added turles

---
+++ Plants/NC/turles.js
@@ -0,0 +1,28 @@
+const {EmbedBuilder} = require("discord.js");
+const db = require("../../index.js");
+module.exports = {
+    name: `turles`, 
+    category: `Night Cap(NC)`,
+    run: async(client, message, args) => {
+        const [result] = await db.query(`SELECT turles from ncdecks`);
+        const turles = new EmbedBuilder()
+            .setTitle(`${result[5].turles}`)   
+            .setDescription(`${result[3].turles}`)
+            .setFooter({text: `${result[2].turles}`})
+            .addFields({
+                name: "Deck Type",
+                value: `${result[6].turles}`,
+                inline: true
+            },{
+                name: "Archetype",
+                value: `${result[0].turles}`,
+                inline: true
+            },{
+                name: "Deck Cost", 
+                value: `${result[1].turles}`,
+                inline: true
+            })
+            .setColor("White")
+            .setImage(`${result[4].turles}`);
+    }
+}

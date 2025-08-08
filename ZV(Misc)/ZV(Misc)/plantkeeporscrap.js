3f98652e6e25d590a60eb409ef8d3f2403e79db7
tbonegaming344 <96313735+tbonegaming344@users.noreply.github.com>
2025-08-08 09:02:41 -0400

    added plantkeeporscrap and zombiekeeporscrap

---
+++ ZV(Misc)/ZV(Misc)/plantkeeporscrap.js
@@ -0,0 +1,102 @@
+const {
+  ContainerBuilder,
+  SectionBuilder,
+  TextDisplayBuilder,
+  ThumbnailBuilder,
+  MessageFlags,
+  SeparatorSpacingSize
+} = require("discord.js");
+const { sep } = require("path");
+module.exports = {
+    name: `plantkeeporscrap`, 
+    aliases: [`plantkeep`, `plantscrap`, `plantcraft`, `plantkos`, `zombierecycle`], 
+    category: `Miscellaneous`,
+    run: async (client, message, args) => {
+        const container = new ContainerBuilder();
+        const tierText1 = new TextDisplayBuilder().setContent([
+          "# Keep or Scrap August 2025 Created By <@256910306003910658>.",
+          "You can comment on [the reddit post](https://www.reddit.com/r/PvZHeroes/comments/1mkf38s/keep_or_scrap_august_2025_explanation_below/#lightbox)"
+        ].join("\n"));
+        const user = await client.users.fetch("256910306003910658");
+        const authorImage = new ThumbnailBuilder().setURL(user.displayAvatarURL());
+        const authorSection = new SectionBuilder()
+          .addTextDisplayComponents(tierText1)
+          .setThumbnailAccessory(authorImage);
+        container.addSectionComponents(authorSection);
+            container.addSeparatorComponents(separator => separator.setSpacing(SeparatorSpacingSize.Large));
+        const tierText2 = new TextDisplayBuilder().setContent([
+          "### Template", 
+          "- **TIERS ARE UNORDERED**",
+          "- **Craft** is a tier I'm bringing back, since it's now a lot more clear which cards are worth putting sparks towards", 
+          "- **Keep** is now two tiers; **Use** and **Keep**. **Use** means that a card is immediately helpful for budget decks while also being valuable to keep for later. **Keep** meanwhile means a card is worth keeping for decks that you'll make later once you own enough higher rarity cards", 
+          "- [tier list templates](https://www.reddit.com/r/PvZHeroes/comments/1lrx6l6/i_made_updated_tier_lists_for_every_class/)"
+        ].join("\n"));
+        container.addTextDisplayComponents(tierText2);
+        container.addSeparatorComponents(separator => separator.setSpacing(SeparatorSpacingSize.Large));
+        const tierText3 = new TextDisplayBuilder().setContent([
+          "# Guardian",
+          "- **Pecanolith** is now Scrap. It's usually been Keepable since Guardian always wanted it for a finisher, but Gravitree is just better top-end that ends up being more lethal without backfiring in the same ways as it. It was still Keepable in February as tech for Quarterly, but now it's just not worth taking", 
+          "- **Hot Date** is Keep tier. It was put into Keepable before since it wasn't useful on a budget, but Keep tier is now a lot more descriptive of the card's limits", 
+          "- **Body-Gourd** and **Soul Patch** are Keepable now. They both got buffs that made the a lot more runnable. Soul Patch is still just a weaker Poppies, but that's not saying it's bad", 
+          "- **Pear Cub** and **Red Stinger** are Use. Again, they got some significant buffs that have made them much more ideal to use"
+        ].join("\n"));
+        const guardianImage = new ThumbnailBuilder().setURL("https://media.discordapp.net/attachments/1044626284346605588/1403342148597715056/guardiankeep.webp?ex=689733b9&is=6895e239&hm=5ce35684b0948ec1da3e2d88e07e2ce55ecb6905fc9925ac1eab639c941541f3&=&format=webp")
+        const guardianSection = new SectionBuilder()
+          .addTextDisplayComponents(tierText3)
+          .setThumbnailAccessory(guardianImage);
+          container.addSectionComponents(guardianSection);
+       container.addSeparatorComponents(separator => separator.setSpacing(SeparatorSpacingSize.Large));
+        const tierText4 = new TextDisplayBuilder().setContent([
+          "# Kabloom",
+          "- A lot of cards are Scrap now; specifically **Banana Launcher**, **Reincarnation**, **Pineclone**, and **Gloom-Shroom**. **Gloom** and **Pineclone** were already bad cards that stopped being useful as budget options due to power-creep. **Blauncher** and **Rein** meanwhile are just too niche to keep on a budget. You typically either want better aggro options or more sparks to put towards **Fig**", 
+          "- **Astro-Shroom** is just Keepable now. It's still a fine budget option and you still want it for Cycle Cap, but Nightcap at max is a lot more flexible and other heroes just don't bother with this card"
+        ].join("\n"));
+        const kabloomImage = new ThumbnailBuilder().setURL("https://media.discordapp.net/attachments/1044626284346605588/1403343799068921957/kabloomkeep.webp?ex=68973542&is=6895e3c2&hm=6aadf5f0b2df73487976346bb871bcad24fed79b3ea65f717e15ca5cb4d9bfda&=&format=webp&width=991&height=873")
+        const kabloomSection = new SectionBuilder()
+          .addTextDisplayComponents(tierText4)
+          .setThumbnailAccessory(kabloomImage);
+        container.addSectionComponents(kabloomSection);
+   container.addSeparatorComponents(separator => separator.setSpacing(SeparatorSpacingSize.Large));
+      const tierText5 = new TextDisplayBuilder().setContent([
+        "# Mega-Grow", 
+        "- Most obviously, **Espresso Fiesta** is craft now. The card is genuinely broken on some heroes, and it's generally really good now", 
+        "- **Lily of the Valley** and **Apotatosaurus** are now Keep and Use respectively. **Apotato** is good top-end that can help finish games, while **Lily** is more consistently good in decks 10k sparks and up after its buff", 
+        "- **Black-Eyed Pea** is Scrap now. Card is genuinely not good anymore ever since Cabbage got its buff. Pea Patch also takes its spot in decks, and Umbrella being more playable means BEP isn't necessary for trick tech", 
+        "-  **Spinach** is Keepable now, since a lot of Leafy cards got buffs. It's like a more versatile Onion Rings with weaker effects"
+      ].join("\n"));
+      const megaGrowImage = new ThumbnailBuilder().setURL("https://media.discordapp.net/attachments/1044626284346605588/1403345073915301958/megagrowkeep.webp?ex=68973672&is=6895e4f2&hm=bdbb4568d2d8a4dbd48bb62430cb9903331fa719d2f8ec39e79cc7a5155246b0&=&format=webp")
+      const megaGrowSection = new SectionBuilder()
+        .addTextDisplayComponents(tierText5)
+        .setThumbnailAccessory(megaGrowImage);
+      container.addSectionComponents(megaGrowSection);
+   container.addSeparatorComponents(separator => separator.setSpacing(SeparatorSpacingSize.Large));
+      const tierText6 = new TextDisplayBuilder().setContent([
+        "# Smarty", 
+        "- Many of the Amphibious cards are Keepable or Scrap now, including **Laser Cattail**, **Bog of Enlightenment**, and **Spyris**. They're used far more rarely at maximum and Smarty in general is very rarely played aggressively", 
+        "- **Sportacus** was moved down and **Dark Matter Dragonfruit** was moved up for similar reasons, as Smarty has more of a focus on midrange and control than before", 
+        "- **Winter Melon** also got moved to Keepable for those reasons"
+      ].join("\n"));
+      const smartyImage = new ThumbnailBuilder().setURL("https://media.discordapp.net/attachments/1044626284346605588/1403346012537356420/smartykeep.webp?ex=68973752&is=6895e5d2&hm=d03651539aca5b8b2a5995b484449463d03b95fe3fabfac265a9b724a8f91827&=&format=webp&width=991&height=873")
+      const smartySection = new SectionBuilder()
+        .addTextDisplayComponents(tierText6)
+        .setThumbnailAccessory(smartyImage);
+      container.addSectionComponents(smartySection);
+      container.addSeparatorComponents(separator => separator.setSpacing(SeparatorSpacingSize.Large));
+      const tierText7 = new TextDisplayBuilder().setContent([
+        "# Solar", 
+        "- **Sun-Shroom is craft**. I feel like this doesn't need an explanation; card's busted", 
+        "- **Aloesaurus** and **Wing-Nut** are Scrap. Similar to Pecan, they were really only recommended for the meta at that time. Aloe made up for the lack of lethality Solar had, while Wing-Nut was a simple answer to Quarterly. Both cards are completely unnecessary to keep now, however", 
+        "- **Cross-Pollination** is Keepable now. It's a decent sustain option with highroll, and wasn't initially recommended due to it seeming underwhelming"
+      ].join("\n"));
+      const solarImage = new ThumbnailBuilder().setURL("https://media.discordapp.net/attachments/1044626284346605588/1403346860780945428/solarkeep.webp?ex=6897381c&is=6895e69c&hm=1bd7e9c3cec58676da49b72cd68921c61c9794a17171d792580c6126d4b7d414&=&format=webp&width=991&height=873")
+      const solarSection = new SectionBuilder()
+        .addTextDisplayComponents(tierText7)
+        .setThumbnailAccessory(solarImage);
+      container.addSectionComponents(solarSection);
+      container.setAccentColor(65280);
+      await message.channel.send({
+      components: [container],
+      flags: MessageFlags.IsComponentsV2,
+    });
+    }
+  }
---
+++ ZV(Misc)/ZV(Misc)/zombiekeeporscrap.js
@@ -0,0 +1,96 @@
+  const {
+  ContainerBuilder,
+  SectionBuilder,
+  TextDisplayBuilder,
+  ThumbnailBuilder,
+  MessageFlags,
+  SeparatorSpacingSize
+} = require("discord.js");
+module.exports = {
+    name: `zombiekeeporscrap`, 
+    aliases: [`zombiekeep`, `zombiescrap`, `zombiecraft`, `zombiekos`, `zombierecycle`], 
+    category: `Miscellaneous`,
+    run: async (client, message, args) => {
+        const container = new ContainerBuilder();
+        const tierText1 = new TextDisplayBuilder().setContent([
+          "# Keep or Scrap For Zombies August 2025 Created By <@256910306003910658>.",
+          "You can comment on [the reddit post](https://www.reddit.com/r/PvZHeroes/comments/1mkf38s/keep_or_scrap_august_2025_explanation_below/#lightbox)"
+        ].join("\n"));
+        const user = await client.users.fetch("256910306003910658");
+        const authorImage = new ThumbnailBuilder().setURL(user.displayAvatarURL());
+        const authorSection = new SectionBuilder()
+          .addTextDisplayComponents(tierText1)
+          .setThumbnailAccessory(authorImage);
+        container.addSectionComponents(authorSection);
+        container.addSeparatorComponents(separator => separator.setSpacing(SeparatorSpacingSize.Large));
+         const tierText2 = new TextDisplayBuilder().setContent([
+                  "### Template", 
+                  "- **TIERS ARE UNORDERED**",
+                  "- **Craft** is a tier I'm bringing back, since it's now a lot more clear which cards are worth putting sparks towards", 
+                  "- **Keep** is now two tiers; **Use** and **Keep**. **Use** means that a card is immediately helpful for budget decks while also being valuable to keep for later. **Keep** meanwhile means a card is worth keeping for decks that you'll make later once you own enough higher rarity cards", 
+                  "- [tier list templates](https://www.reddit.com/r/PvZHeroes/comments/1lrx6l6/i_made_updated_tier_lists_for_every_class/)"
+                ].join("\n"));
+                container.addTextDisplayComponents(tierText2);
+                container.addSeparatorComponents(separator => separator.setSpacing(SeparatorSpacingSize.Large));
+  const tierText8 = new TextDisplayBuilder().setContent([
+        "# Beastly",
+        "- **Hover-Goat**, **Maniacal Laughter**, **Hunting Grounds**, **Interstellar Bounty Hunter**, **Gargantuar-Throwing Gargantuar**, **Zombot 1000**, and **Fraidy Cat** are all Scrap now. They were all weak cards to recommend in the first place, and the meta shifting to zombie tempo has made cards like Maniacal and IBH way worse", 
+        "- **Secret Agent** is now Keepable. It's not ran as often as before due to the aforementioned meta shift"
+      ].join("\n"));
+      const beastlyImage = new ThumbnailBuilder().setURL("https://media.discordapp.net/attachments/1044626284346605588/1403347640690933920/beastlykeep.webp?ex=689738d6&is=6895e756&hm=ae977ce831f155e7ec11f3f8a2940809a493fca36b9566ee15bf2cd7e4660eee&=&format=webp&width=991&height=873")
+      const beastlySection = new SectionBuilder()
+        .addTextDisplayComponents(tierText8)
+        .setThumbnailAccessory(beastlyImage);
+      container.addSectionComponents(beastlySection);
+   container.addSeparatorComponents(separator => separator.setSpacing(SeparatorSpacingSize.Large));
+      const tierText9 = new TextDisplayBuilder().setContent([
+        "# Brainy",
+        "- **Shield-Crusher Viking** is Use now. It's a decent budget option that does see use in max decks, so it's typically worth keeping"
+      ].join("\n"));
+      const brainyImage = new ThumbnailBuilder().setURL("https://media.discordapp.net/attachments/1044626284346605588/1403348049329127495/brainykeep.webp?ex=68973937&is=6895e7b7&hm=dcf77c0f0a30bfdb45655efc021a0d3fbf8d01abc5eae8600ed744a518e8d67c&=&format=webp&width=991&height=873")
+      const brainySection = new SectionBuilder()
+        .addTextDisplayComponents(tierText9)
+        .setThumbnailAccessory(brainyImage);
+      container.addSectionComponents(brainySection);
+   container.addSeparatorComponents(separator => separator.setSpacing(SeparatorSpacingSize.Large));
+      const tierText10 = new TextDisplayBuilder().setContent([
+        "# Crazy",
+        "- **Barrel of Deadbeards** is Keepable. It's super niche now since you just don't run it in aggro anymore. Other decks tend to not want BoD since Mission is weak and BoD itself messes with your tempo", 
+        "- **Imp-Throwing Gargantuar** is scrap. Even with it costing 4, it's still pretty slow and not worth taking. It doesn't really max into anything besides Trickstache either, so keeping it on that basis is also not worth it"
+      ].join("\n"));
+      const crazyImage = new ThumbnailBuilder().setURL("https://media.discordapp.net/attachments/1044626284346605588/1403348852567838730/crazykeep.webp?ex=689739f7&is=6895e877&hm=f9378551c619afb20e2a4ab7d068b56cf9451f73f83b24cf16fdc83d9184c905&=&format=webp")
+      const crazySection = new SectionBuilder()
+        .addTextDisplayComponents(tierText10)
+        .setThumbnailAccessory(crazyImage);
+      container.addSectionComponents(crazySection);
+   container.addSeparatorComponents(separator => separator.setSpacing(SeparatorSpacingSize.Large));
+      const tierText11 = new TextDisplayBuilder().setContent([
+        "# Hearty",
+        "- **Zombology Teacher** and **Genetic Experiment** are in Keep and Use respectively. They were underrated at the time for being potentially outclassed by Hearty's new tempo options, but Genetic is still really useful and Teacher is still wanted in maxed decks",
+        "- **Jurassic Fossilhead**, **All-Star Zombie**, and **Coffee Zombie** are Scrap. Colosseum, Sumo, and Primeval Yeti are all much more important and useful cards, and once you've unlocked them, then you're putting the sparks towards cards like Viral and Black Hole. By that point, they're hardly even relevant options anymore"
+      ].join("\n"));
+      const heartyImage = new ThumbnailBuilder().setURL("https://media.discordapp.net/attachments/1044626284346605588/1403349706616344586/heartykeep.webp?ex=68973ac3&is=6895e943&hm=8692a77bf88af276b2d47c978699d73d8eae4c00fe9010dd9c6f80c6d5623d5a&=&format=webp&width=991&height=873")
+      const heartySection = new SectionBuilder()
+        .addTextDisplayComponents(tierText11)
+        .setThumbnailAccessory(heartyImage);
+      container.addSectionComponents(heartySection);
+   container.addSeparatorComponents(separator => separator.setSpacing(SeparatorSpacingSize.Large));
+      const tierText12 = new TextDisplayBuilder().setContent([
+        "# Sneaky", 
+        "- **Imp-Throwing Imp**, **Toxic Waste Imp**, and **Imposter** are Keepable now. Imps as a strategy is rarely ever played on other heroes besides HG anyway, and even on HG, they're relatively weaker due to Imp Commander's change", 
+        "- **Laser Base Alpha** is Keepable. It's... fine, I guess. Usable on a budget if you have nothing better, and maxes out on Impfinity now", 
+        "- **Mixed-Up Gravedigger** and **Pogo Bouncer** are now Keep and Use respectively. Pogo is a lot less niche of a card people had been giving it credit for up until now, while MUG has been seeing more use in these slower Sneaky decks", 
+        "- **Zombot Plank Walker**, **Excavator Zombie**, and **Tomb Raiser Zombie** are all scrap. Plankwalker is hard to justify on a budget since Quarterly is so useful on SB and no other heroes can really use this card. Excavator was Keepable for Neptuna, but she hardly runs it now. Tomb Raiser was just overhyped, honestly; it's still an easily controllable 4-drop that needs to hit face to get value"
+      ].join("\n"));
+      const sneakyImage = new ThumbnailBuilder().setURL("https://media.discordapp.net/attachments/1044626284346605588/1403350906749063271/sneakykeep.webp?ex=68973be1&is=6895ea61&hm=32ee57218052274b1fc067ceb3094c5ef670a786ed32959ebdf42876e75018c8&=&format=webp")
+      const sneakySection = new SectionBuilder()
+        .addTextDisplayComponents(tierText12)
+        .setThumbnailAccessory(sneakyImage);
+      container.addSectionComponents(sneakySection);
+      container.setAccentColor(10494192);
+        await message.channel.send({
+      components: [container],
+      flags: MessageFlags.IsComponentsV2,
+    });
+    }
+}
